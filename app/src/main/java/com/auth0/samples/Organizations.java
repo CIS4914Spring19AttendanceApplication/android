142e51c98cd303671b12d4549f4a8a7a053b68e6
mcm2100 <mcm2100@ufl.edu>
2019-03-30 16:25:03 -0400

    Buttons/Screens

--- app/src/main/java/com/auth0/samples/Organizations.java
+++ app/src/main/java/com/auth0/samples/Organizations.java
@@ -1,10 +1,13 @@
 package com.auth0.samples;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.squareup.okhttp.Callback;
@@ -28,7 +31,7 @@ public class Organizations extends Activity {
     TextView headerText;
     private static final String API_URL = "https://rollcall-api.herokuapp.com/api/user/get/";
     private static String name = "";
-    private String lets;
+    private static String email = "";
 
 
     @Override
@@ -40,10 +43,18 @@ public class Organizations extends Activity {
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setHasFixedSize(true);
 
+        Button qr = (Button) findViewById(R.id.signedIn);
+        qr.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showNextActivity(QRScanner.class);
+            }
+        });
+
 
         final String accessToken = getIntent().getStringExtra(EXTRA_ACCESS_TOKEN);
         final String email = getIntent().getStringExtra("USER_EMAIL");
-        //final String name = getIntent().getStringExtra("USER_NAME");
+        final String name = getIntent().getStringExtra("USER_NAME");
 
                             OkHttpClient client = new OkHttpClient();
                             Request request = new Request.Builder()
@@ -67,21 +78,17 @@ public class Organizations extends Activity {
                                             String jsonData = response.body().string();
                                             JSONObject reader = new JSONObject(jsonData);
                                             JSONArray Jarray = reader.getJSONArray("enrollments");
-                                            Log.d("asdfas", jsonData);
-                                            Log.d("asdasdf", Jarray.toString());
                                             final String Orgs[] = new String[Jarray.length()];
                                             for (int i=0; i<Jarray.length(); i++){
                                                 JSONObject object = Jarray.getJSONObject(i);
                                                 Orgs[i] = object.getString("organization");
                                             }
-                                            Log.d("qsadg", Orgs[1]);
                                             runOnUiThread(new Runnable() {
                                                 @Override
                                                 public void run() {
                                                     recyclerView.setAdapter(new Adapter(Organizations.this, Orgs));
                                                 }
                                             });
-                                            name = reader.getString("first_name");
                                         } catch (JSONException a) {
                                             a.printStackTrace();
                                         }
@@ -99,5 +106,13 @@ public class Organizations extends Activity {
                                 }
                             });
 
+    }
+    private void showNextActivity(Class next) {
+        Intent intent = new Intent(Organizations.this, next);
+        intent.putExtra(EXTRA_ACCESS_TOKEN, getIntent().getStringExtra(EXTRA_ACCESS_TOKEN));
+        intent.putExtra("USER_EMAIL", email);
+        intent.putExtra("USER_NAME",name);
+        startActivity(intent);
+        finish();
     }
 }
